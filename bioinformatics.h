8f1d08b4602690c95a01950f19c0c2a61ea5642b
ambtomagos <ambtomagos@gmail.com>
2016-03-12 07:50:21 +0800

    added getSkew function

--- bioinformatics.h
+++ bioinformatics.h
@@ -5,10 +5,6 @@ int getHammingDistance(char string1[], char string2[]){
 	if(strlen(string1) != strlen(string2)) return -1;
 
 	else{
-		for(i=0;i<strlen(string1);i++){
-			tolower(string1[i]);
-			tolower(string2[i]);
-		}
 
 		for(i=0;i<strlen(string1);i++){
 			count += (string1[i]!=string2[i]) ? 1 : 0;
@@ -36,7 +32,12 @@ int isValidString(char s1[], char s2[]){
 	return 1;
 }
 int getSkew(char string[], int x){
-	return 0;
+	int i,g=0,c=0;
+	for(i=0; i<x;i++){
+		if(string[i]=='G') g+=1;
+		else if(string[i]=='C') c+=1;
+	}
+	return g-c;
 }
 int getMaxSkewN(char s1[], int x){
 	return 0;
